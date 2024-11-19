24b402d8efc0c448393b2fd439a3533053c3ef0c
ChyaseHaresh <thisisharesh@gmail.com>
2024-11-19 16:52:17 +0545

    cors added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app= express();
 app.use(express.json());
 
 app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://employee-management-back.vercel.app/?vercelToolbarCode=z5KiUTWckvtn5Mg' }));
 
 dotenv.config({path:'./.env'});
 
